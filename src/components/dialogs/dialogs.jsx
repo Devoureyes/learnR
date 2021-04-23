c02e750b99c8f07be11502a9b5a060479237889a
Andrew_Sokolov <devoureyes@yandex.ru>
2021-04-23 19:26:52 +0300

    диалоги

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -1,7 +1,35 @@
+import s from './dialog.module.css'
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (p) => {
+    return (
+        <div className={s.dialog + '' + s.active}>
+            <NavLink to={`/messages/${p.n}`}>
+                {p.name}
+            </NavLink>
+        </div>
+    )
+}
+
+const Messagge = (p) => {
+    return (
+        <div className={s.messages}>{p.m}</div>
+    )
+}
+
 const Dialogs = () => {
     return (
-        <div>
-            Dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <DialogItem n="1" name="Andrew"/>
+                <DialogItem n="2" name="Andy"/>
+                <DialogItem n="3" name="Andrew_bot"/>
+            </div>
+            <div className={s.messages}>
+                <Messagge m="hi"/>
+                <Messagge m="yo"/>
+                <Messagge m="lol"/>
+            </div>
         </div>
     )
 }
