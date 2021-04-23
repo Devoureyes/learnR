0f76f61b980760643b39b013619bcc77bd488a5e
Andrew_Sokolov <devoureyes@yandex.ru>
2021-04-23 22:26:19 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/post";
 import React from "react";
 
+let p = [
+    {id: 1, message: 'Now look at this'},
+    {id: 2, message: 'Today, i\'m went to the park and ate a hamburger'},
+    {id: 3, message: 'Hello! It\'s my first post'},
+]
+
 const MyPosts = () => {
     return (
         <div className={s.posts}>
@@ -9,9 +15,7 @@ const MyPosts = () => {
                 <textarea/>
                 <button className={`myButton ${s.but}`}>Add new post</button>
             </div>
-            <Post message='Now look at this' />
-            <Post message="Today, i'm went to the park and ate a hamburger" />
-            <Post message="Hello! It's my first post" />
+            {p.map((el) => <Post message={el.message}/>)}
         </div>
     )
 }
