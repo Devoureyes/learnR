63d117eea3174e8aac79328de0fe281d95707656
Andrew_Sokolov <devoureyes@yandex.ru>
2021-07-26 20:02:07 +0300

    new

---
+++ src/components/todo/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+    return (<div style={{display: 'flex', justifyContent: 'center', margin: '.5em'}}>
+        <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>)
+}
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import TodoList from "./TodoList";
 import Context from "../../redux/Context";
-import AddTodo from "./AddTodo";
+//import AddTodo from "./AddTodo";
+import Loader from "./Loader";
+import Modal from "./modal/Modal";
 
 const styles = {
-    h1: { margin: 0},
-    h3: { color: '#d5f51f'}
+    h1: {margin: 0},
+    h3: {fontSize: '50px',color: '#d5f51f', userSelect: 'none'}
 }
-
-
+const AddTodo = React.lazy(() => import('./AddTodo'))
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+
     React.useEffect(() => {
         /*http://mepnew/api/test*/
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-        .then(response => response.json())
-        .then(todos => {
-            setTodos(todos)
-        })
-    })
+        fetch('http://mepnew/api/test')
+            .then(response => response.json())
+            .then(todos => {
+                setTodos(todos)
+                setLoading(false)
+            })
+    },[])
 
-    let [todos, setTodos] = React.useState([
-        { id: 1, completed: false, title: 'Купить молоко'},
-        { id: 2, completed: true, title: 'Купить хлеб'},
-        { id: 3, completed: false, title: 'Купить масло'},
-    ])
+    let [todos, setTodos] = React.useState([/*
+        {id: 1, completed: false, title: 'Купить молоко'},
+        {id: 2, completed: true, title: 'Купить хлеб'},
+        {id: 3, completed: false, title: 'Купить масло'},
+    */])
+    const [loading, setLoading] = React.useState(true)
 
-    function toggleTodo (id) {
+
+    function toggleTodo(id) {
         setTodos(todos.map(todo => {
             if (todo.id === id) {
                 todo.completed = !todo.completed
@@ -51,8 +56,16 @@ export default () => {
     return (<Context.Provider value={{removeTodo}}>
         <div>
             <h1 style={styles.h1}>Todo tutorial</h1>
-            <AddTodo onCreate={addTodo}/>
-            {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <h3 style={styles.h3}>No TODO's</h3> }
+            <Modal />
+            <React.Suspense fallback={<p>Loading...f</p>}>
+                <AddTodo onCreate={addTodo}/>
+            </React.Suspense>
+            {loading && <Loader />}
+            {todos.length ?
+                (<TodoList todos={todos} onToggle={toggleTodo}/>)
+                :
+                loading ? null :(<h3 style={styles.h3}>No TODO's</h3>)
+            }
         </div>
     </Context.Provider>)
 }
