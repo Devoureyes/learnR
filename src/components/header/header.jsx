ee3ca7847cb3acfbfb24f4c4b9b1c52b6bd9d224
Andrew_Sokolov <devoureyes@yandex.ru>
2021-08-26 00:28:41 +0300

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import s from "./header.module.css"
 import {NavLink} from "react-router-dom";
-
-const Header = (p) => {
+import cn from 'classnames'
+const Header = ({isAuth,logout}) => {
     return (
-        <header className={`${s.header} style`}>
+        <header className={cn(s.header,'style')}>
             <div className={s.logo}>
                 <img src="https://static.vecteezy.com/system/resources/previews/001/191/986/non_2x/circle-logo-png.png" alt=""/>
             </div>
-            <div className={`${s.name} ${s.area}`}><p>Devoureyes.online</p></div>
+            <div className={cn(s.name, s.area)}><p>Devoureyes.online</p></div>
             <div className={s.login}>
-                { p.isAuth ? <button className={s.logout} onClick={p.logout}>Logout</button> :<NavLink className={s.linkLogin} to={'/login'}>LOGIN</NavLink>}
+                {isAuth ? <button className={s.logout} onClick={logout}>Logout</button> :<NavLink className={s.linkLogin} to={'/login'}>LOGIN</NavLink>}
             </div>
         </header>
     )
